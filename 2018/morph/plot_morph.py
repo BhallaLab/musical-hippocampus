9a276267c0aab4e72028dad8858e44c239948a4b
Dilawar Singh <dilawars@ncbs.res.in>
2018-11-02 16:08:07 +0530

    Updated.

--- 2018/morph/plot_morph.py
+++ 2018/morph/plot_morph.py
@@ -25,7 +25,7 @@ background_ = 0
 h_, w_      = 480, 800
 canvas_     = np.zeros( shape=(h_,w_,3) ) + background_
 win_        = cv2.namedWindow( "NRN" )
-hippoImg_   = cv2.imread( os.path.join( sdir_, 'hippocampus-800x480.png' ) )
+hippoImg_   = cv2.imread( os.path.join( sdir_, 'hippocampus-800x480-1.png' ) )
 
 ca1_ = [ 
         ((377, 129),   210, './swcs/cell1-11b-CA1.CNG.swc' ),
