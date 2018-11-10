c4223f93f100e916c82b51c75f61bfedc66ef98e
Dilawar Singh <dilawars@ncbs.res.in>
2018-11-10 12:02:30 +0530

    Added wavfile and play scripts.

--- play.py
+++ play.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
 import sys
-import sound
-sound.play_seq( map(int, sys.argv[1:]) )
+import os
+import subprocess
+import time
+
+def play(n, delay=0.05):
+    wavfile = os.path.join( 'wav', '%s.wav' % n )
+    subprocess.Popen( [ 'aplay', wavfile ] )
+    time.sleep(delay)
+
+if len(sys.argv) == 2:
+    play( sys.argv[1] )
+else:
+    for n in sys.argv[1:]:
+        play( sys.argv[1], 0.5 )
